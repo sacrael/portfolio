a39ae81eb914a368c621dafa03c6adf1b8ff2aca
Abdul-Muiz Yusuff <muiz.rpi@gmail.com>
2020-01-03 09:42:34 -0500

    Added vladmir to my project list

--- src/components/contact.js
+++ src/components/contact.js
@@ -3,12 +3,27 @@ import {withRouter} from 'react-router-dom'
 
 class Contact extends React.Component<{},{}> {
 
+  constructor(props) {
+    super(props)
+    this.emailRef = React.createRef()
+  }
+
+  sendMail = () => {
+    if (this.emailRef.current) {
+
+        let body_ = this.emailRef.current.value.split(" ").join("+")
+        window.open(`mailto:muiz.rpi@gmail.com?subject=[muiz.dev] Contact&body=${this.emailRef.current.value}`);
+    }
+  }
+
+
+
   render () {
     return (<div>
         <div className="page-header">Contact</div>
         <div className="email-input"><input type="text" placeholder="contact@email.com" /></div>
-        <div className="email-textarea"><textarea placeholder="email..."></textarea></div>
-        <div className="email-submit"><button className="magic-btn">SEND</button></div>
+        <div className="email-textarea"><textarea placeholder="email..." ref={this.emailRef}></textarea></div>
+        <div className="email-submit"><button className="magic-btn" onClick={() => {this.sendMail()}}>SEND</button></div>
 
       </div>);
   }
